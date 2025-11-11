15515c13de24220afae768b5ed7ba20dd1cb49fe
M-Itrych <michal.itrych1312@gmail.com>
2025-11-11 15:18:40 +0100

    navbar/components init

--- app/components/global/NavBar/navbar.tsx
+++ app/components/global/NavBar/navbar.tsx
@@ -1,6 +1,6 @@
 export default function Navbar() {
     return (
-        <div>
+        <div className="flex justify-between items-center p-4 w-[80px]">
             <h1>Navbar</h1>
         </div>
     );

2bf2fe9437fe8ce6537a877bc3ea6f71485bf137
Kagamiel <kagamielwarframe@wp.pl>
2025-11-12 20:01:49 +0100

    Poprawka zakladki Ogloszenia

--- app/(pages)/Parent/layout.tsx
+++ app/(pages)/Parent/layout.tsx
@@ -1,13 +1,13 @@
 import type { ReactNode } from 'react';
 import Topbar from '../../components/global/NavBar/parenttopbar';
-import Navbar from '../../components/global/NavBar/navbar';
+import NavbarTemplate from '../../components/global/NavBar/navbar_template';
 
 export default function ParentLayout({ children }: { children: ReactNode }) {
   return (
     <div className="h-screen flex flex-col bg-blue-50 overflow-hidden">
       <Topbar />
       <div className="flex flex-1 overflow-hidden min-h-0">
-        <Navbar />
+        <NavbarTemplate items={[]} />
         <main className="flex-1 overflow-y-auto bg-blue-50 min-w-0">
           {children}
         </main>
